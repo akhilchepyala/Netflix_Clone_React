2134e4351362402d17806a708fc93ba649dbd1b9
Akhil Chepyala <89103033+akhilchepyala@users.noreply.github.com>
2024-04-10 21:02:30 -0500

    Added TailWind and Removed unnecessary depencdency

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="text-5xl font-bold text-green-800">
-        <p>Hello Netflix GPT</p>
-    </div>
-  );
+    const [name, setName] = useState('akhil');
+
+
+    return (
+        <div className="text-5xl font-bold text-green-800">
+            <p>Hello Netflix GPT</p>
+        </div>
+    );
 }
 
 export default App;
